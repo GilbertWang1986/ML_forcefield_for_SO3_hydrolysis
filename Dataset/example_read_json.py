87ad4420729ed5e842e6ae23c40e6eb879cc66d6
Chao Wang <wangchaohi@gmail.com>
2023-03-20 22:13:17 +0800

    init

---
+++ Dataset/example_read_json.py
@@ -0,0 +1,23 @@
+import json
+from ase import Atoms
+from ase import Atom
+import numpy as np
+
+
+def json2atoms(jsonstr):
+    dict = json.loads(jsonstr)
+    symbols = ''
+    for _i in dict['symbols']:
+        symbols += _i
+    atoms = Atoms(symbols, positions=dict['positions'])
+    return atoms
+
+
+frames = []
+with open('DataSet.json') as f:
+    sn = 0
+    while True:
+        line = f.readline()
+        if len(line) == 0: break
+        atoms = json2atoms(line)
+        frames.append(atoms)
